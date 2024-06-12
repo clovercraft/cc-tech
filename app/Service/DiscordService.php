4c18d4f9393c42047f95d68700690308635d72d7
Martin Sheeks <martin@traction.codes>
2024-06-12 17:57:54 -0500

    fix: cleanup of DiscordService after API split

--- app/Service/DiscordService.php
+++ app/Service/DiscordService.php
@@ -2,31 +2,13 @@
 
 namespace App\Service;
 
-use App\Models\AppSetting;
 use App\Models\Member;
 use App\Service\Discord\BotRequest;
-use Illuminate\Http\Client\Response;
-use Illuminate\Support\Carbon;
 use Illuminate\Support\Collection;
-use Illuminate\Support\Facades\Http;
-use Illuminate\Support\Facades\Log;
 use Laravel\Socialite\Facades\Socialite;
 
 class DiscordService
 {
-
-    private const API_URL = 'https://discord.com/api/v10';
-    private const API_VERSION = 10;
-
-    private string $authToken;
-    private string $refreshToken;
-
-    public function __construct()
-    {
-        $this->getAuthToken(true);
-        $this->getRefreshToken(true);
-    }
-
     public function authorize()
     {
         // https://discord.com/api/oauth2/authorize?client_id=1160334357899264021&permissions=8&response_type=code&redirect_uri=https%3A%2F%2Flocalhost%2Foauth%2Freturn&scope=bot+guilds.members.read+guilds
@@ -39,64 +21,9 @@ class DiscordService
             ->redirect();
     }
 
-    public function has_authorization(): bool
-    {
-        // check token is set
-        $token = $this->getAuthToken(true);
-        if (empty($token)) {
-            return false;
-        }
-
-        // check status
-        $authentication = $this->checkAppStatus();
-        return $authentication->has('user');
-    }
-
     public function botInviteLink(): string
     {
         return sprintf("https://discord.com/oauth2/authorize?client_id=%s&scope=bot&permissions=8", env('DISCORD_CLIENT_ID'));
-    }
-
-    /**
-     * I never got ths to work, going to just check auth status instead.
-     * @deprecated 0.0
-     *
-     * @return void
-     */
-    public function refresh_authorization()
-    {
-        $refresh_token = $this->getRefreshToken();
-        $client_id = env('DISCORD_CLIENT_ID');
-        $client_secret = env('DISCORD_CLIENT_SECRET');
-        $auth = "Basic " . $client_id . ":" . $client_secret;
-
-        $data = [
-            'grant_type'    => 'refresh_token',
-            'refresh_token' => $refresh_token,
-        ];
-
-        $uri = 'https://discord.com/api/v10/oauth2/token';
-        Log::info("Attempting Discord token refresh", [$uri, $auth, $data]);
-        // $response = Http::withHeaders([
-        //     "Authorization" => $auth,
-        //     "Content-Type"  => "application/x-www-form-urlencoded"
-        // ])
-        //     ->post($uri, $data);
-        $response = Http::withBasicAuth($client_id, $client_secret)
-            ->contentType('application/x-www-form-urlencoded')
-            ->post($uri, $data);
-        if ($response->failed()) {
-            dd($response->body());
-        }
-        // $user = Socialite::driver('discord')->userFromToken($this->getAuthToken());
-
-        return $response;
-    }
-
-    public function checkAppStatus(): Collection
-    {
-        $response = $this->get('/oauth2/@me');
-        return $response->collect();
     }
 
     public function get_members(): Collection|bool
@@ -107,7 +34,6 @@ class DiscordService
 
         $count = 50;
         $limit = 50;
-        $after = '';
         $args = ['limit' => 100];
 
         while ($count >= $limit) {
@@ -119,7 +45,11 @@ class DiscordService
             $members = $members->merge($entries);
             $count = $entries->count();
             if ($count >= $limit) {
-                $args['after'] = $this->parseLastUser($entries);
+                $users = $entries->filter(function ($entry) {
+                    return key_exists('user', $entry);
+                });
+                $last = $users->last();
+                $args['after'] = $last['user']['id'];
             }
         }
 
@@ -175,58 +105,5 @@ class DiscordService
         // create ban
 
         return true;
-    }
-
-    private function parseLastUser(Collection $entries): string
-    {
-        $users = $entries->filter(function ($entry) {
-            return key_exists('user', $entry);
-        });
-        $last = $users->last();
-        return $last['user']['id'];
-    }
-
-    public function getAuthToken(bool $refresh = false): string
-    {
-        if ($refresh || !isset($this->authToken)) {
-            $this->authToken = AppSetting::access('discord_token');
-        }
-        return $this->authToken;
-    }
-
-    public function getRefreshToken(bool $refresh = false): string
-    {
-        if ($refresh || !isset($this->refreshToken)) {
-            $this->refreshToken = AppSetting::access('discord_refresh_token');
-        }
-        return $this->refreshToken;
-    }
-
-    private function get(string $path, array $query_string = []): Response|bool
-    {
-        $uri = $this->request_url($path);
-        $response = Http::withToken($this->getAuthToken())
-            ->withQueryParameters($query_string)
-            ->get($uri);
-        if ($response->failed()) {
-            Log::warning("Discord API call failed.", [
-                'acting_as' => 'USER',
-                'method'    => 'GET',
-                'uri'       => $uri,
-                'response'  => $response
-            ]);
-            return false;
-        }
-        return $response;
-    }
-
-    private function request_url(string $path = ''): string
-    {
-        $uri = self::API_URL;
-        // if (!str_contains($path, 'oauth2')) {
-        //     $uri .= self::API_VERSION;
-        // }
-        $uri .= $path;
-        return $uri;
     }
 }
